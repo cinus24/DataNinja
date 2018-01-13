a569e09ff65acef14eb3a5c153eed89b52b40d36
Adam <adambrych@gmail.com>
2018-01-13 14:42:18 +0100

    ads

--- DataNinja/DataReader/NewAdsReader.py
+++ DataNinja/DataReader/NewAdsReader.py
@@ -3,20 +3,74 @@ import codecs
 import DataNinja.DataReader.Config as Config
 import pandas as pd
 
-column_names = ["id","region_id","category_id","subregion_id","district_id","city_id","accurate_location","user_id","sorting_date","created_at_first","valid_to","title","description","full_description","has_phone","params","private_business","has_person","photo_sizes","paidads_id_index","paidads_valid_to","predict_sold","predict_replies","predict_views","reply_call","reply_sms","reply_chat","reply_call_intent","reply_chat_intent"]
+column_names = ["id", "region_id", "category_id", "subregion_id", "district_id", "city_id", "accurate_location", "user_id",
+                "sorting_date", "created_at_first", "valid_to", "title", "description", "full_description", "has_phone",
+                "params", "private_business", "has_person", "photo_sizes", "paidads_id_index", "paidads_valid_to",
+                "predict_sold", "predict_replies", "predict_views", "reply_call", "reply_sms", "reply_chat",
+                "reply_call_intent", "reply_chat_intent"]
+files = ['ads_2016_11_01', 'ads_2016_12_01', 'ads_2017_01_01', 'ads_2017_02_01', 'ads_2017_03_01', 'ads_2017_04_01',
+         'ads_2017_05_01', 'ads_2017_06_01', 'ads_2017_07_01', 'ads_2017_08_01', 'ads_2017_09_01']
 
 def get_ads_from_one_month():
-    path = 'ads_2016_12_01'
+    path = 'ads_2016_11_01'
     file = codecs.open(Config.ADS_DATA_CATALOG + "\\" + path, encoding='utf8')
     counter = 0
-    file_write = open("ads_2016_12_01.txt", "w", encoding='utf8')
+    file_write = open("ads_2016_11_01.txt", "w", encoding='utf8')
     for line in file:
         line = line.replace("\n", "")
         line = line.replace("<STREET>", "")
         line = line.replace("<CITY>", "")
         line = line.replace("<POSTCODE>", "")
         line = line.replace("<URL>", "")
-        line = re.sub('[^0-9a-zA-Z",.?:;{}\-_+=!@#$%^&* ąęółżź()[]/\\\]+', '', line)
+        line = line.replace("<PHONE>", "")
+        line = line.replace("ą", "a")
+        line = line.replace("ę", "e")
+        line = line.replace("ó", "o")
+        line = line.replace("ś", "s")
+        line = line.replace("ł", "l")
+        line = line.replace("ż", "z")
+        line = line.replace("ź", "z")
+        line = line.replace("ć", "c")
+        line = line.replace("ń", "n")
+
+        line = line.replace("Ą", "A")
+        line = line.replace("Ę", "E")
+        line = line.replace("Ó", "O")
+        line = line.replace("Ś", "S")
+        line = line.replace("Ł", "L")
+        line = line.replace("Ż", "Z")
+        line = line.replace("Ź", "Z")
+        line = line.replace("Ć", "C")
+        line = line.replace("Ń", "N")
+        line = line.replace("-", " ")
+        line = line.replace("{", " ")
+        line = line.replace("}", " ")
+        line = line.replace(".", " ")
+        line = line.replace("_", " ")
+        line = line.replace("/", " ")
+        line = line.replace("\\", " ")
+        line = line.replace("(", " ")
+        line = line.replace(")", " ")
+        line = line.replace("[", " ")
+        line = line.replace("]", " ")
+        line = line.replace(":", " ")
+        line = line.replace(";", " ")
+        line = line.replace("?", " ")
+        line = line.replace("+", " ")
+        line = line.replace("=", " ")
+        line = line.replace("!", " ")
+        line = line.replace("#", " ")
+        line = line.replace("$", " ")
+        line = line.replace("^", " ")
+        line = line.replace("&", " ")
+        line = line.replace("@", " ")
+        line = line.replace("<", " ")
+        line = line.replace(">", " ")
+        #line = re.sub('[^0-9a-zA-Z", ]+', '', line)
+        if line == " ":
+            continue
+        if line == "":
+            continue
         if line != "" and not line.isspace() and counter > 0:
             file_write.write(line)
         counter += 1
@@ -24,8 +78,9 @@ def get_ads_from_one_month():
     file.close()
 
 def to_dataframe():
-    df = pd.read_csv('ads_2016_12_01.txt', sep=",", header=None, names=column_names)
-    print(df.iloc[0].full_description)
+    df = pd.read_csv('ads_2016_11_01.txt', sep=",", header=None, names=column_names)
+    print(df.iloc[0])
+
 
 get_ads_from_one_month()
 to_dataframe()
