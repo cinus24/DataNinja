1353ee3305a9ad2d0bfe9377928053160b9a308d
Adam <adambrych@gmail.com>
2018-01-13 12:35:03 +0100

    read ads

--- DataNinja/DataReader/NewAdsReader.py
+++ DataNinja/DataReader/NewAdsReader.py
@@ -6,10 +6,10 @@ import pandas as pd
 column_names = ["id","region_id","category_id","subregion_id","district_id","city_id","accurate_location","user_id","sorting_date","created_at_first","valid_to","title","description","full_description","has_phone","params","private_business","has_person","photo_sizes","paidads_id_index","paidads_valid_to","predict_sold","predict_replies","predict_views","reply_call","reply_sms","reply_chat","reply_call_intent","reply_chat_intent"]
 
 def get_ads_from_one_month():
-    path = 'ads_2016_11_01'
+    path = 'ads_2016_12_01'
     file = codecs.open(Config.ADS_DATA_CATALOG + "\\" + path, encoding='utf8')
     counter = 0
-    file_write = open("test.txt", "w", encoding='utf8')
+    file_write = open("ads_2016_12_01.txt", "w", encoding='utf8')
     for line in file:
         line = line.replace("\n", "")
         line = line.replace("<STREET>", "")
@@ -24,8 +24,8 @@ def get_ads_from_one_month():
     file.close()
 
 def to_dataframe():
-    df = pd.read_csv('test.txt', sep=",", header=None, names=column_names)
+    df = pd.read_csv('ads_2016_12_01.txt', sep=",", header=None, names=column_names)
     print(df.iloc[0].full_description)
 
-#get_ads_from_one_month()
+get_ads_from_one_month()
 to_dataframe()
